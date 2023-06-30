3838f293fe11cade6f9da09febaf59ab398b8b45
Slouchwind <2241051890@qq.com>
2023-07-01 03:32:09 +0800

    index

--- components/i18n/config/indexConfig.ts
+++ components/i18n/config/indexConfig.ts
@@ -1,7 +1,8 @@
 const index = {
     selectingText: {
         'zh-CN': '请选择栏目。',
-        'zh-TW': '請選擇欄目。'
+        'zh-TW': '請選擇欄目。',
+        'en-US': 'Please select a task',
     }
 }
 export default index;
